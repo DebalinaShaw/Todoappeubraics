76846bb710114204829a035186f9f45ded0de2db
Debalina shaw <debolina059@gmail.com>
2023-07-12 12:37:24 +0530

    updated

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool ({ 
-    user: "postgres",
-    password: "0800",
-    host: "localhost",
+    user: "perntodo_imvj_user",
+    password: "EtJkolpp1daMj45eMOHupp7FIonGRK17",
+    host: "dpg-cin4c415rnur6sc0eqp0-a",
     port: 5432,
-    database: "perntodo"
+    database: "perntodo_imvj"
 });
 
 module.exports = pool;
